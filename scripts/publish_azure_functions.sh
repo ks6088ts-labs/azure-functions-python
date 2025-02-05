84a0e42843dc19a632d30d1c2592b980a815a4b3
ks6088ts <ks6088ts@gmail.com>
2025-02-05 11:03:00 +0900

    add validation

--- scripts/publish_azure_functions.sh
+++ scripts/publish_azure_functions.sh
@@ -2,7 +2,10 @@
 
 set -eux
 
-# Environment variables
+# check if FUNCTION_APP_NAME is set
+if [ -z ${FUNCTION_APP_NAME+x} ]; then echo "FUNCTION_APP_NAME is unset"; exit 1; fi
+
+# environment variables
 uv export --format requirements-txt --no-dev --no-hashes --output-file requirements.txt
 # remove -e . from requirements.txt
 sed -i '/^-e .*/d' requirements.txt
